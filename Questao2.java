8c8c664029654b9305c094fb58eb68c082e7813e
GuilhermeBenevides <78943497+GuilhermeBenevides@users.noreply.github.com>
2021-02-15 10:19:20 -0300

    Add files via upload

---
+++ Questao2.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+public class Questao2 {
+	@Test
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Questão 2:");
+		System.out.println("Hello World");  
+	}
+
+}
